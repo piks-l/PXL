fefa84dfb8a290121c1c2e46bd1f4bcf91773a96
PuxianAlHazred <yoann@piks-l.com>
2022-01-24 08:17:44 +0100

    no loading

--- components/ui/logo.vue
+++ components/ui/logo.vue
@@ -5,17 +5,37 @@
 </template>
 <style lang="postcss">
   .logo {
-     @apply font-garamond text-2xl leading-[100px];
+     @apply font-garamond text-[25px] leading-[100px];
   }
 </style>
 <script>
 export default {
   methods: {
+    scrollLogo() {
+      let limitBottom = document.documentElement.offsetHeight - window.innerHeight;
+      let GSAP = this.$gsap;
 
+      window.addEventListener("scroll",function(){
+        if(document.documentElement.scrollTop === 0){
+          GSAP.to('.logo', {fontSize: "100px", duration: 1, delay:0 ,ease: 'power2'})
+        }
+        if(document.documentElement.scrollTop > 50 && document.documentElement.scrollTop < 60 ){
+          GSAP.to('.logo', {fontSize: "25px", duration: 1, delay:0 ,ease: 'power2'})
+        }
+        if(document.documentElement.scrollTop > (limitBottom - 100) && document.documentElement.scrollTop < (limitBottom - 50) ){
+          GSAP.to('.logo', {fontSize: "25px", duration: 1, delay:0 ,ease: 'power2'})
+        }
+        if(document.documentElement.scrollTop === limitBottom){
+          GSAP.to('.logo', {fontSize: "25px", duration: 1, delay:0, ease: 'power2'})
+        }
+      })
+
+    },
   },
   created() {
   },
   mounted() {
+    this.scrollLogo();
     if(this.$store.state.preloading === false) {
       //console.log("IORE 1")
     } else {
